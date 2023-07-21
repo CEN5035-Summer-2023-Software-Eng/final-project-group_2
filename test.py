15e9b030b716ffafeaa7f9feba4d55bcb3419f52
czuv <czuvieta2016@fau.edu>
2023-07-20 23:09:41 -0400

    Python file with flask to run index.html

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+import uuid
+import base64
+import json
+from flask import Flask, request, render_template, jsonify, send_file
+from io import BytesIO
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5678)
