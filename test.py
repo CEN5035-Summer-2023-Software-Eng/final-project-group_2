5c7f832e3313c17fd02c788a64320c2c9e773e75
czuv <czuvieta2016@fau.edu>
2023-07-21 12:49:01 -0400

    Added redirects

--- test.py
+++ test.py
@@ -14,5 +14,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(host="localhost", port=5678)
